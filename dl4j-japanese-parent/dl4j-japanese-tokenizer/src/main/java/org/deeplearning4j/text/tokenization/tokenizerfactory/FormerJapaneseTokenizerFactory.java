299bca693c6d75d6fd1bb48038445b910ad1be42
unknown <s-zhang@Galileo.advanced-media.co.jp>
2015-10-01 17:08:50 +0900

    [add] support for kuromoji-ipadic 0.9.0 [change] JapaneseTokenizer and JapaneseTokenizerFactory for kuromoji-0.7.7 are renamed by using prefix "Former"

--- dl4j-japanese-parent/dl4j-japanese-tokenizer/src/main/java/org/deeplearning4j/text/tokenization/tokenizerfactory/FormerJapaneseTokenizerFactory.java
+++ dl4j-japanese-parent/dl4j-japanese-tokenizer/src/main/java/org/deeplearning4j/text/tokenization/tokenizerfactory/FormerJapaneseTokenizerFactory.java
@@ -2,31 +2,32 @@ package org.deeplearning4j.text.tokenization.tokenizerfactory;
 
 import org.deeplearning4j.text.tokenization.tokenizer.TokenPreProcess;
 import org.deeplearning4j.text.tokenization.tokenizer.Tokenizer;
-import org.deeplearning4j.text.tokenization.tokenizer.JapaneseTokenizer;
-
+import org.deeplearning4j.text.tokenization.tokenizer.FormerJapaneseTokenizer;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 
 import java.io.InputStream;
 
-public class JapaneseTokenizerFactory implements TokenizerFactory {
-  private com.atilika.kuromoji.ipadic.Tokenizer tokenizer;
+public class FormerJapaneseTokenizerFactory implements TokenizerFactory {
+  private org.atilika.kuromoji.Tokenizer tokenizer;
+  private TokenPreProcess preProcess;
   private boolean useBaseForm;
-  private TokenPreProcess tokenPreProcess;
 
-  public JapaneseTokenizerFactory() {
+  public FormerJapaneseTokenizerFactory() {
     this(
-      com.atilika.kuromoji.TokenizerBase.Mode.NORMAL,
-      false
+            org.atilika.kuromoji.Tokenizer.builder().mode(org.atilika.kuromoji.Tokenizer.Mode.NORMAL).build(),
+            false
     );
   }
 
-  public JapaneseTokenizerFactory(com.atilika.kuromoji.TokenizerBase.Mode mode, boolean useBaseForm) {
-	  this.useBaseForm = useBaseForm;
-	  this.tokenizer = new com.atilika.kuromoji.ipadic.Tokenizer.Builder().mode(mode).build();
+  public FormerJapaneseTokenizerFactory(org.atilika.kuromoji.Tokenizer.Mode mode, boolean useBaseForm) {
+    this(
+            org.atilika.kuromoji.Tokenizer.builder().mode(mode).build(),
+            useBaseForm
+    );
   }
 
   // If you want further customization, you can give a raw kuromoji's tokenizer.
-  public JapaneseTokenizerFactory(com.atilika.kuromoji.ipadic.Tokenizer tokenizer, boolean useBaseForm) {
+  public FormerJapaneseTokenizerFactory(org.atilika.kuromoji.Tokenizer tokenizer, boolean useBaseForm) {
     this.tokenizer = tokenizer;
     this.useBaseForm = useBaseForm;
   }
@@ -36,15 +37,10 @@ public class JapaneseTokenizerFactory implements TokenizerFactory {
     if (toTokenize == null || toTokenize.isEmpty()) {
       throw new IllegalArgumentException("Unable to proceed; no sentence to tokenize");
     }
-    Tokenizer ret = new JapaneseTokenizer(tokenizer, toTokenize, useBaseForm);
-    if(tokenPreProcess != null) {
-    	ret.setTokenPreProcessor(tokenPreProcess);
-    }
-
+    Tokenizer ret = new FormerJapaneseTokenizer(tokenizer, toTokenize, useBaseForm);
     return ret;
   }
 
-  // By using kuromoji, inputstream is not be supported yet
   @Override
   public Tokenizer create(InputStream toTokenize) {
     throw new UnsupportedOperationException();
@@ -52,7 +48,7 @@ public class JapaneseTokenizerFactory implements TokenizerFactory {
 
   @Override
   public void setTokenPreProcessor(TokenPreProcess preProcessor) {
-	  this.tokenPreProcess = preProcessor;
+    this.preProcess = preProcess;
   }
 
 }
